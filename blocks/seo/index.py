3c54c68a9d3797b02172f683bf8a891337ff4c46
Sam Scherf <csmoran@protonmail.com>
2021-06-04 02:36:05 -0400

    Added settings and updated seo block

--- blocks/seo/index.py
+++ blocks/seo/index.py
@@ -7,13 +7,22 @@ from dominate.util import raw
 
 def main(data):
 
-    from blockable import load_css
+    from blockable import load_css, site_data
+
+    # Get data
+    settings = site_data("settings/meta")
     font_link = "https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap"
 
     HTML = dominate.document(title=data["title"])
     with HTML.head:
+        # Add css
         raw(load_css("assets/css/stylesheet.css"))
         link(href=font_link, rel="stylesheet")
+
+        # Add favicon
+        link(rel="icon", _type="image/icon type", href=settings["favicon"])
+
+        # set general meta tags
         meta(charset="utf-8")
         meta(name="viewport", content="width=device-width, user-scalable=no")
 
--- layouts/homepage/index.py
+++ layouts/homepage/index.py
@@ -7,20 +7,20 @@ def main(data):
     # Import blocks and assets
     from blockable import blocks
 
-    # Start document and add css
+    # Start document and open
     HTML = blocks.seo(data["seo"])
+    with HTML:
+        # Add cover
+        div(blocks.cover(data["cover"]), _id="cover")
 
-    # Add cover
-    HTML += blocks.cover(data["cover"])
+        # Create container and add blocks
+        with div(_class="container mx-auto py-3", _id="content"):
+            div(blocks.about(data["about"]), _class="py-3", _id="about")
+            div(blocks.key_points(data["key_points"]), _class="py-3", _id="key_points")
+            div(blocks.price_cards(data["price_cards"]), _class="py-3", _id="pricing")
 
-    # Create container and add blocks
-    container = HTML.add(div(_class="container mx-auto py-3", _id="content"))
-    container += div(blocks.about(data["about"]), _class="py-3", _id="about")
-    container += div(blocks.key_points(data["key_points"]), _class="py-3", _id="key_points")
-    container += div(blocks.price_cards(data["price_cards"]), _class="py-3", _id="pricing")
-
-    # Add contact form
-    HTML += div(blocks.contact({}), _id="contact")
+        # Add contact form
+        div(blocks.contact({}), _id="contact")
 
     # Return html
     return HTML
