ccdd7426d426b175f4ed154b97d54d35c53f92f6
Sam <sam@ampolic.com>
2021-06-13 17:44:42 +0000

    More google appeasement

--- blocks/key_points/index.py
+++ blocks/key_points/index.py
@@ -8,18 +8,23 @@ def main(data):
     # Import css loader
     from blockable import load_css
 
-    # Create div and add css
+    # Create div
     HTML = div(_class="grid")
-    HTML += raw(load_css("blocks/key_points/stylesheet.css"))
+    with HTML:
+        # Add css
+        HTML += raw(load_css("blocks/key_points/stylesheet.css"))
 
-    for point in data["key_points"]:
-        point_div = HTML.add(div(_class="flex"))
-        image_div = point_div.add(div(_class="image-div py-3"))
-        text_div = point_div.add(div(_class="text-div py-3"))
+        # Loop though points
+        for point in data["key_points"]:
+            # Point div
+            with div(_class="flex"):
+                # Image div
+                with div(_class="image-div py-3"):
+                    img(src=point["icon"], width="70px", height="70px")
 
-        image_div += img(src=point["icon"], _class="key-icon")
-
-        text_div += h3(point["title"])
-        text_div += p(point["description"])
+                # Text div
+                with div(_class="text-div py-3"):
+                    h3(point["title"])
+                    p(point["description"])
 
     return HTML
