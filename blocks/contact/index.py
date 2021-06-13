233f6667c7864fb8d3847ff0476f6be87e5b9a9c
Sam <sam@ampolic.com>
2021-06-13 16:02:56 +0000

    Added form labels

--- blocks/contact/index.py
+++ blocks/contact/index.py
@@ -6,11 +6,11 @@ This file implements a simple contact form
 
 # Import dominate
 from dominate.util import raw
-from dominate.tags import div, p, h2, form, _input, textarea, script
+from dominate.tags import div, p, h2, form, _input, label, textarea, script
 
 
 def main(data):
-    """ get contact form html
+    """ contact form html
 
     """
 
@@ -31,65 +31,91 @@ def main(data):
             p(_id="form-response")
 
             # Create html form
-            with form(_class="form-grid mx-auto contact-div", _id="form", onsubmit="submitForm(event)"):
-                # Add first and last _id
+            form_attr = {
+                "_class": "form-grid mx-auto contact-div",
+                "_id": "form",
+                "onsubmit": "submitForm(event)",
+            }
+            with form(**form_attr):
+                # Two columns
                 with div(_class="grid"):
                     # First name
                     with div(_class="form-row"):
+                        label(
+                            "First Name",
+                            _class="hidden",
+                            _for="first_name",
+                            )
                         _input(
-                                placeholder="First Name",
-                                _type="text",
-                                _class="text-input my-2",
-                                _id="first_name",
-                                name="first_name",
-                                required=True,
-                                )
+                            placeholder="First Name",
+                            _type="text",
+                            _class="text-input my-2",
+                            _id="first_name",
+                            name="first_name",
+                            required=True,
+                            )
                     # Last name
                     with div(_class="form-row"):
+                        label(
+                            "Last Name",
+                            _class="hidden",
+                            _for="last_name",
+                            )
                         _input(
-                                placeholder="Last Name",
-                                _type="text",
-                                _class="text-input my-2",
-                                _id="last_name",
-                                name="last_name",
-                                required=True,
-                                )
+                            placeholder="Last Name",
+                            _type="text",
+                            _class="text-input my-2",
+                            _id="last_name",
+                            name="last_name",
+                            required=True,
+                            )
 
                 # Add email
                 with div(_class="form-row"):
+                    label(
+                        "Email",
+                        _class="hidden",
+                        _for="email",
+                        )
                     _input(
-                            placeholder="Email",
-                            _type="text",
-                            _class="text-input my-2",
-                            _id="email",
-                            name="email",
-                            required=True,
-                            )
+                        placeholder="Email",
+                        _type="text",
+                        _class="text-input my-2",
+                        _id="email",
+                        name="email",
+                        required=True,
+                        )
 
                 # Add message
                 with div(_class="form-row"):
+                    label(
+                        "Message",
+                        _class="hidden",
+                        _for="message",
+                        )
                     textarea(
-                            placeholder="Message",
-                            _type="text",
-                            rows="6",
-                            _class="text-input my-2",
-                            _id="message",
-                            name="message",
-                            required=True,
-                            )
+                        placeholder="Message",
+                        _type="text",
+                        rows="6",
+                        _class="text-input my-2",
+                        _id="message",
+                        name="message",
+                        required=True,
+                        )
 
                 # Add h-captcha
                 with div(_class="form-row mx-auto py-2"):
-                    div(_class="h-captcha", data_sitekey="d827d0bd-2dc5-4981-9784-b837691433b8")
+                    div(
+                        _class="h-captcha",
+                        data_sitekey="d827d0bd-2dc5-4981-9784-b837691433b8",
+                        )
 
                 # Add submit button
                 with div(_class="form-row"):
                     _input(
-                            value="Submit",
-                            _type="submit",
-                            # _type="button",
-                            # onclick="submitForm()",
-                            _class="my-2 submit-button",
-                            )
+                        value="Submit",
+                        _type="submit",
+                        _class="my-2 submit-button",
+                        )
 
     return HTML
