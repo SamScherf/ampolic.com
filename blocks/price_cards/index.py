cb25196ac84df4b8df8941df4642b4f174ea5422
Sam Scherf <csmoran@protonmail.com>
2021-09-16 09:37:20 -0700

    Reworked Price Card block and added section header

--- blocks/price_cards/index.py
+++ blocks/price_cards/index.py
@@ -1,29 +1,36 @@
-
-# Import dominate
+from dominate.tags import div, small, h2, h3, h4, p, a, button
 from dominate.util import raw
-from dominate.tags import div, small, h3, h4, p, a, button
 
 
 def main(data):
 
     # Import blockable modules
-    from blockable import load_css, load_js
+    from blockable import load_css
 
     # Create div and add css
-    HTML = div(_class="grid-container")
-    HTML += raw(load_css("blocks/price_cards/stylesheet.css"))
-    card_grid = HTML.add(div(_class="price-grid"))
+    with div(_class="grid-container") as HTML:
+        raw(load_css("blocks/price_cards/stylesheet.css"))
 
-    for card in data["cards"]:
-        # Create top level divs
-        card_div = card_grid.add(div(_class="card text-center mx-auto my-3"))
-        card_header = card_div.add(div(_class="card-header"))
-        card_body = card_div.add(div(_class="card-body"))
+        # Add section header
+        with div(_class="py-3 text-center"):
+            h2(data["header"])
+            p(data["sub_header"])
 
-        card_header += h3(card["label"], _class="h4 my-0")
+        # Card grid
+        with div(_class="price-grid"):
 
-        card_body += h4(card["price"], small("/mo", _class="muted-text"), _class="h2")
-        card_body += p(raw(card["description"].replace("\n", "<br>")), _class="fake-list")
-        card_body += a(button("Contact Us", _class="price-button"), href="#contact", _class="no_line")
+            # Create div for each card
+            for card in data["cards"]:
+                with div(_class="card text-center mx-auto my-3"):
+
+                    # Card header
+                    with div(_class="card-header"):
+                        h3(card["label"], _class="h4 my-0")
+
+                    # Card body
+                    with div(_class="card-body"):
+                        h4(card["price"], small("/mo", _class="muted-text"), _class="h2")
+                        p(raw(card["description"].replace("\n", "<br>")), _class="fake-list")
+                        a(button("Contact Us", _class="price-button"), href="#contact", _class="no_line")
 
     return HTML
